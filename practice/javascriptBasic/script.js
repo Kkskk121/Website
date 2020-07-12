5f597e1bc06096f5099a441d5df2755842e6c7fa
Katie Kim <katherine.kim@columbia.edu>
2020-07-12 09:38:39 -0700

    Starting JS Basics

---
+++ practice/javascriptBasic/script.js
@@ -0,0 +1,7 @@
+
+var firstName = prompt("Nice to meet you, what's your first name: " );
+var lastName = prompt("What's your last name: " );
+alert("Nice to meet you " + firstName + " " + lastName);
+console.log("Also great to meet you, " + firstName + " " + lastName);
+var age = prompt("What is your age? ");
+console.log("You are "+ age + " years old.");
