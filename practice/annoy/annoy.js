230fd89d97afa7f2fec3e6038e71d3d7ccbc616d
Katie Kim <katherine.kim@columbia.edu>
2020-07-12 12:46:39 -0700

    Javascript loops

---
+++ practice/annoy/annoy.js
@@ -0,0 +1,9 @@
+
+
+var answer = prompt("Are we there yet? ");
+
+while (answer.indexOf("yes") == "-1" && answer !== "yah" && answer !== "yeah"){
+    var answer = prompt("Are we there yet? ");
+}
+
+alert("YAY, we made it!! When are we eating? ");
